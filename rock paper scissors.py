bc3cf5ff8f6eb1f1ab08d2057ff627a763d97555
amirreza khajeh <71586685+amirreza1387@users.noreply.github.com>
2023-07-23 13:46:03 +0330

    Update rock paper scissors.py
    جا به جا کردن مرز های برنامه نویسی

--- rock paper scissors.py
+++ rock paper scissors.py
@@ -1,5 +1,6 @@
 countlog = 0
-while True :
+couantall=0
+while couantall<=0 :
     #_______variable_________#
     import random
 
@@ -14,97 +15,62 @@ while True :
     user= ("dayan")
     passw=("dayan password")
     #_______function_______#
-
+     
     quastion=input("do you have an account? yes/no ")
-   
+     
     while True:
-        if quastion == ("yes"):
-           userr= input("username :")
-           passrr= input("password :")
-           if userr==user and passrr==passw:
-                print("login succesful")
-                break           
-           
-            
-           if userr!=user and passrr!=passw: 
-                print("login unsiccessfull")  
-                countlog += 1 
-                print (f"{countlog}/ 3")
-                if countlog >= 3:
-                    
-                  break
-                    
-                    
-        if quastion == ("no"):
-         print ("""
-          ------------
-        [ register page ]
-          -------------
-         """)
-         print ("create an account")
-         user1= input("create username :")
-         passw1= input("create password :")
+     if quastion == ("yes"):
+      userr= input("username :")
+      passrr= input("password :")
+      if userr==user and passrr==passw:
         
-        
-
-
-
-
-
-
-
-
-
-        count = int(input("sar chanta??"))
+        print("login succesful")
+      
+        count= int(input("sar chanta????"))
         while True :
-                        
-                        player_ch= input("""
+                         
+            player_ch= input("""
 
-                        chose rock , paper or scissors : """)
+            chose rock , paper or scissors : """)
 
-                        computer_ch= random.choice(options)
+            computer_ch= random.choice(options)
 
-                        print ("""you chose:""" , player_ch)
-                        print ("""opponet chose:""" , computer_ch)
+            print ("""you chose:""" , player_ch)
+            print ("""opponet chose:""" , computer_ch)
                     
                     #_______if________#
-                        if player_ch==computer_ch :
-                            print("round raw!  ")
-                            print (f"{player}:{cmp}")
+            if player_ch==computer_ch :
+             print("round raw!  ")
+             print (f"{player}:{cmp}")
                         
 
-                        elif player_ch==("rock") and computer_ch==("paper") or player_ch==("scissors") and computer_ch==("rock") or player_ch==("paper") and computer_ch==("scissors") :
-                            print(" you lost the round ! ") 
+            elif player_ch==("rock") and computer_ch==("paper") or player_ch==("scissors") and computer_ch==("rock") or player_ch==("paper") and computer_ch==("scissors") :
+                print(" you lost the round ! ") 
                         
-                            cmp  += 1
-                            print (f"{player}:{cmp}")
+                cmp  += 1
+                print (f"{player}:{cmp}")
 
 
-                        elif computer_ch==("rock") and player_ch==("paper") or computer_ch==("scissors") and player_ch==("rock") or computer_ch==("paper") and player_ch==("scissors"):
-                            print (" you won the round !")  
-                            player += 1
-                            print (f"{player}:{cmp}")
+            elif computer_ch==("rock") and player_ch==("paper") or computer_ch==("scissors") and player_ch==("rock") or computer_ch==("paper") and player_ch==("scissors"):
+                print (" you won the round !")  
+                player += 1
+                print (f"{player}:{cmp}")
                         
 
-                        else :
-                            print ("enter  vaild choise ")
+            else :
+             print ("enter  vaild choise ")
                         
 
                     #_______if2________#
-                        if player==count :
-                            print (f"""you won the mach {player}:{cmp}  
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        """)
-                            break
+            if player==count :
+              print (f"you won the mach {player}:{cmp} ") 
+              break
+                      
+                  
+            
 
-                        elif cmp==count :
-                            print (f"""
+            elif cmp==count :
+             print (f"""
                             
                             
                             
@@ -113,8 +79,32 @@ while True :
                             
                             
                             
-                            you lost the mach {player}:{cmp} """)
-                            break
+                you lost the mach {player}:{cmp} """)
+             break        
+        
+        break           
+     if  userr!=user or passrr!=passw: 
+        print("login unsiccessfull")  
+        countlog += 1 
+        couantall += 1
+        print (f"{countlog}/ 3")
+        if countlog >= 3:
+                
+                  break   
+            
+      
+                    
+                        
+        if quastion == ("no"):
+                     print ("""
+                     ------------
+                     [ register page ]
+                 -------------
+                         """)
+                     print ("create an account")
+                     user1= input("create username :")
+                     passw1= input("create password :")
+       
 
 
 
